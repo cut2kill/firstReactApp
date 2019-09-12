8cdde0e84ae410581d52750c0b3097b21983d3ce
cut2kill <47491973+cut2kill@users.noreply.github.com>
2019-09-12 21:37:36 +0500

    Initial commit

---
+++ components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+
+const CURRENCIES: string[]
+const CURRENCIES = ["RUB","USD","EUR"];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div className="App">...</div>
+  }
+}
